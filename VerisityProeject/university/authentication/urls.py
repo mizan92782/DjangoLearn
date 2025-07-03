609ec07040a2dbd41cf84db0b6ecb2b902424196
mizanur rahman <mizanmd92782@gmail.com>
2025-07-03 20:56:39 +0600

    adding 'user define authentication form'

--- VerisityProeject/university/authentication/urls.py
+++ VerisityProeject/university/authentication/urls.py
@@ -2,4 +2,5 @@ import django.urls
 from . import views
 urlpatterns = [
   django.urls.path('', views.authentication_form, name='auth_form'),
+  django.urls.path('dauth', views.DefineAuthForm, name='define_auth_form'),
   ]
--- VerisityProeject/university/authentication/views.py
+++ VerisityProeject/university/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import DefineUserForm
 
 # Create your views here.
 def authentication_form(request):
@@ -14,4 +15,16 @@ def authentication_form(request):
   
     
    return render(request, 'authentication/authentication.html',{'form': frm})
-    
+
+
+
+def DefineAuthForm(request):
+   if request.method=='POST':
+      frm = DefineUserForm(request.POST)
+      if frm.is_valid():
+         frm.save();
+         print("user crated succedfully")
+   else:
+      frm = DefineUserForm()
+      print("user not created")
+   return render(request, 'authentication/define_auth.html', {'form': frm})
